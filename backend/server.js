555e5a69bfa070e51ddff79376bf50b930b2d94e
Ajims Ismail <ajimsismail123@gmail.com>
2025-08-05 23:26:21 +0530

    App backend configurations setuped and DB connected

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`server running on port  ${PORT}`));
+
